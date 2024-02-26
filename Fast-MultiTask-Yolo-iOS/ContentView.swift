35a05a1ae5edc1d9fc6f2d7a76cc9f495277fa7f
Hlibisev <hlibisev@gmail.com>
2024-02-26 10:38:11 +0700

    Initial Commit

---
+++ Fast-MultiTask-Yolo-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fast-MultiTask-Yolo-iOS
+//
+//  Created by Anton Fonin on 26.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
